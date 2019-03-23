9557bfb5509a2e5f7d262e72b193c76794e6ab1b
jeeinn <thinkwei2012@gmail.com>
2019-03-23 13:55:26 +0800

    增加360doc自动点击

--- contents.js
+++ contents.js
@@ -4,12 +4,13 @@
 
 (function () {
     let url = window.location.href;
-    let timeDelay = 2 * 1000;
-    let timeDelayDefault = timeDelay;
+    let timeDelay = 0;
+    let timeDelayDefault = 2 * 1000;
+    let removeReadMore = {};
 
     // 读取设定的延迟结果
     chrome.storage.sync.get(['time_delay'], function (result) {
-        timeDelay = result.time_delay ? result.time_delay : timeDelayDefault;
+        timeDelay = result.time_delay ? (result.time_delay * 1000) : timeDelayDefault;
     });
 
     // 所有具体规则
@@ -17,29 +18,37 @@
         // CSDN & ITeye
         case /(http|https):\/\/blog\.csdn\.net\/.*/.test(url):
         case /(http|https):\/\/.*\.iteye\.com\/.*/.test(url):
-            setTimeout(function () {
+            removeReadMore = function (){
                 let canClick = document.querySelector("#btn-readmore");
                 if (canClick) canClick.click();
-            }, timeDelay);
+            };
             break;
         case /(http|https):\/\/bbs\.csdn\.net\/.*/.test(url):
-            setTimeout(function () {
+            removeReadMore = function () {
                 let canClick = document.querySelector(".js_show_topic");
                 if (canClick) canClick.click();
-            }, timeDelay);
+            };
             break;
         // 百度知道
         case /(http|https):\/\/zhidao\.baidu\.com\/.*/.test(url):
-            setTimeout(function () {
+            removeReadMore = function () {
                 let canClick = document.querySelector(".wgt-answers-showbtn");
                 if (canClick) canClick.click();
 
                 canClick = document.querySelector(".wgt-best-showbtn");
                 if (canClick) canClick.click();
-            }, timeDelay);
+            };
+            break;
+        // 360doc
+        case /(http|https):\/\/.*360doc\.com\/content\/.*/.test(url):
+            removeReadMore = function() {
+                document.getElementsByTagName('body')[0].classList.remove('articleMaxH');
+                console.log('360doc');
+            };
             break;
         default:
             break;
     }
+    setTimeout(removeReadMore, timeDelay);
 
 })();
